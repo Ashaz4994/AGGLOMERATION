d64f23871dde83cdc40a49d74a302cc6ac88d14d
Naman Kumar <namannk1704@gmail.com>
2025-01-18 23:28:26 +0530

    home

--- aquaguard-frontend/src/App.jsx
+++ aquaguard-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './components/Home/Home'
 import Loader from './components/Loader/Loader'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
       <Home/>
+      <Navbar/>
     </>
   )
 }
